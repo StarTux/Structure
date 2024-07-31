99f71e1f3608c8a55614ef00e877a6b53c10e9c3
StarTux <ths.loy@gmail.com>
2024-07-31 09:48:35 +0200

    Handle structure discovery

--- src/main/java/com/cavetale/structure/StructurePlugin.java
+++ src/main/java/com/cavetale/structure/StructurePlugin.java
@@ -1,12 +1,19 @@
 package com.cavetale.structure;
 
+import com.cavetale.core.event.block.PlayerBlockAbilityQuery;
+import com.cavetale.core.event.structure.PlayerDiscoverStructureEvent;
+import com.cavetale.structure.cache.Structure;
 import com.cavetale.structure.cache.StructureCache;
 import java.util.logging.Logger;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.event.world.WorldLoadEvent;
@@ -59,6 +66,41 @@ public final class StructurePlugin extends JavaPlugin implements Listener {
     @EventHandler
     private void onChunkUnload(ChunkUnloadEvent event) {
         structureCache.onChunkUnload(event.getWorld(), event.getChunk().getX(), event.getChunk().getZ());
+    }
+
+    @EventHandler(ignoreCancelled = false, priority = EventPriority.MONITOR)
+    private void onPlayerInteract(PlayerInteractEvent event) {
+        if (!event.hasBlock()) {
+            return;
+        }
+        final Player player = event.getPlayer();
+        switch (player.getGameMode()) {
+        case SURVIVAL:
+        case ADVENTURE:
+            break;
+        default:
+            return;
+        }
+        switch (event.getAction()) {
+        case RIGHT_CLICK_BLOCK:
+        case LEFT_CLICK_BLOCK:
+            break;
+        default:
+            return;
+        }
+        final Block block = event.getClickedBlock();
+        final Structure structure = structureCache.at(block);
+        if (structure == null || structure.isDiscovered()) {
+            return;
+        }
+        if (!PlayerBlockAbilityQuery.Action.BUILD.query(player, block)) {
+            return;
+        }
+        if (!new PlayerDiscoverStructureEvent(player, structure).callEvent()) {
+            return;
+        }
+        structure.setDiscovered(true);
+        getLogger().info(player.getName() + " discovered " + structure.getWorldName()  + "/" + structure.getId());
     }
 
     public static StructurePlugin structurePlugin() {
